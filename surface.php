99fa83fda50e63252b906b7c2bc19f1aac2d12ea
Gregtmlg <greg.glemot@gmail.com>
2023-02-16 19:41:48 +0100

    Ajoute nav surface + renomme sous-marine.php

---
+++ surface.php
@@ -0,0 +1,35 @@
+<nav>
+    <table border="0" style="width: 100%; padding: 10px; background-color: rgb(1, 1, 78); font-family: Arial, Helvetica, sans-serif; font-weight: bold;" >
+        <tr>
+            <td style="text-align: center; " ><a class="head_nav" href="index.php?page=pa">PA</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=pha">PHA</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=bcr">BCR</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=fremm">FREMM</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=fremm_da">FREMM-DA</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=fasm">FASM</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=fda">FDA</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=faa">FAA</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=flf">FLF</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=fs">FS</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=phm">PHM</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=psp">PSP</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=pag">PAG</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=rpc">RPC</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=cmt">CMT</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=brs">BRS</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=bbpd">BBPD</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=pp">PP</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=bsam">BSAM</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=bsaom">BSAOM</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=bh">BH</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=bho">BHO</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=bem">BEM</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=be">BE</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=rias">RIAS</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=bsaa">BSAA</a></td>
+            <td style="text-align: center; "><a class="head_nav" href="index.php?page=ve">VE</a></td>
+           
+        </tr>
+    </table>
+   
+<nav>
