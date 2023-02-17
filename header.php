f2f384ffdc8d8be4ba40c87c7f306433bff55f11
lolovasa <lolo.vasapolli@hotmail.fr>
2023-02-17 09:26:49 +0100

    ajout logo minmer header

--- header.php
+++ header.php
@@ -3,8 +3,9 @@
         <tr>
             <td style="text-align: center; width: 15%; background-color: white; " ><img class="img30" src="images\LOGO_MARINE_NATIONALE_2021.svg.png" alt="logo Marine Nationale"/></td>
 
-            <td style="text-align: center; "><h1>Livret de la flotte de la Marine Nationale</h1></td>
+            <td style="text-align: center;width: 70% "><h1>Livret de la flotte de la Marine Nationale</h1></td>
             
+            <td style= "text-align: center; width= 15%; background-color:white;"><img src="\images\LOGO_MINMER.svg.png" alt="logo Marine Nationale" width=40% height=40%/></td>
            
         </tr>
     </table>
