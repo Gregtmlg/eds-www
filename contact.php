2b82be5724f3e86528ffa47c836591108c4d2eb2
Gregtmlg <greg.glemot@gmail.com>
2023-02-24 16:58:23 +0100

    Ajout insert contact sql

--- contact.php
+++ contact.php
@@ -1,17 +1,30 @@
 <?php
-  if(isset($_POST['w3review']) and $_POST['w3review']!="  ") {
+  if((isset($_POST['commentaire']) and ($_POST['commentaire']!="  " or $_POST['commentaire']!=""))) {
     echo '<div style="text-align: center; font-family: Impact, Haettenschweiler, sans-serif; font-weight: bold;">
         Merci ';echo  $_POST["fname"]; echo ' '; echo $_POST['lname']; echo' pour votre commentaire !
     </div>';
     echo '<br><br>';
-  }
-  if(isset($_POST['w3review']) and $_POST['w3review']=="  ") {
-    echo '<div style="text-align: center; font-family: Impact, Haettenschweiler, \'Arial Narrow Bold\' , sans-serif; font-weight: bold;">
-    Remplis tous les champs enculé !!!
-</div>';
-echo '<br><br>';
+
+    // paramètres de connexion à la BDD
+    $server = "localhost";
+    $bdd = "bdd_flotte";
+    $user = "root";
+    $passwd  = "";
+
+    // Connexion au serveur
+    $sql_connexion = mysqli_connect($server, $user, $passwd) or die("erreur de connexion au serveur");
+    mysqli_select_db($sql_connexion, $bdd) or die("erreur de connexion a la base de donnees");
+
+    // Creation et envoi de la requete
+    $query = "INSERT into f_contact (c_prenom, c_nom, c_email, c_commentaire) VALUES ('".$_POST['fname']."', '".$_POST['lname']."', '".$_POST['email']."', '".$_POST['commentaire']."')";
+    mysqli_query($sql_connexion, $query);
+
+    
+    mysqli_close($sql_connexion);
+    unset($_POST);
   }
 ?>
+
 
 <br>
 
@@ -22,11 +35,11 @@ echo '<br><br>';
     <label for="lname">Nom:</label>
     <input type="text" id="lname" name="lname"><br><br>
 
-    <!-- <label for="lname">Password:</label>
-    <input type="password" id="lname" name="pwd"><br><br> -->
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" required><br><br>
 
-    <label for="w3review">Commentaire:</label><br>
-    <textarea id="w3review" name="w3review" rows="4" cols="50">  </textarea><br><br>
+    <label for="commentaire">Commentaire:</label><br>
+    <textarea id="commentaire" name="commentaire" rows="4" cols="50" required>  </textarea><br><br>
 
     <input type="submit" value="Submit"><br><br>
 
